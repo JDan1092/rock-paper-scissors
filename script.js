5a5b2699edf9daebc8255ae3db5de566492279be
JDan1092 <95508276+JDan1092@users.noreply.github.com>
2023-12-01 15:49:06 -0600

    game completed through rps-ui

--- script.js
+++ script.js
@@ -1,72 +1,134 @@
-const options = ["rock", "paper", "scissors"];
+//const options = ["rock", "paper", "scissors"];
 
-function getComputerChoice(){
-    const choice = options[Math.floor(Math.random() * options.length)];
-    return choice;
-}
-
-function checkWinner(playerSelection, computerSelection){
-    if(playerSelection == computerSelection){
-        return "Tie";
-    } else if (
-        (playerSelection == "rock" && computerSelection == "scissors") ||
-        (playerSelection == "scissors" && computerSelection == "paper") ||
-        (playerSelection == "paper" && computerSelection == "rock")
-    ){
-        return "Player";
+function getComputerChoice() {
+    let rock = "Rock";
+    let paper = "Paper";
+    let scissors = "Scissors";
+    let getRandomValue = Math.random();
+    if (getRandomValue <= 0.33) {
+        return rock;
+    } else if (getRandomValue <= 0.66) {
+        return paper;
     } else {
-        return "Computer";
+        return scissors;
     }
 }
 
-function playRound(playerSelection, computerSelection){
-    const result = checkWinner(playerSelection, computerSelection);
-    if(result == "Tie"){
-        return "It's a tie!"
-    } else if(result == "Player"){
-        return "Human is the winner!"
-    } else{
-        return "Computer conquered you!"
-    }
-}
+function game() {
+    let humanWin = 0;
+    let computerWin = 0;
+    let gameWinner = "";
 
-function getPlayerChoice(){
-    let validatedInput = false;
-    while(validatedInput == false){
-        const choice = prompt("Rock Paper Scissors");
-        if(choice == null){
-            continue;
+    const buttons = document.querySelectorAll('button');
+    buttons.forEach((button) => {
+        button.addEventListener('click', () => {
+            playerSelection = button.className;
+            const computerSelection = getComputerChoice();
+            battleWinText.textContent = (playRound(playerSelection, computerSelection));
+            humanWinText.textContent = "Human total wins: " + humanWin;
+            computerWinText.textContent = "Computer total wins: " + computerWin;
+            endGame();
+        })
+    })
+
+    function playRound(playerSelection, computerSelection){
+        let tie = "It's a tie";
+        let rockBeatScissors = "Human chose wisely!";
+        let scissorsBeatPaper = "Human chose wisely!";
+        let paperBeatRock = "Human chose wisely!";
+        let rockBeatScissorsLoss = "Computer is laughing of you!";
+        let scissorsBeatPaperLoss = "Computer is laughing of you!";
+        let paperBeatRockLoss = "Computer is laughing of you!";
+
+        if (playerSelection === computerSelection) {
+            return tie;
+        } else if (playerSelection === "Rock" && computerSelection === "Scissors")  {
+            humanWin++;
+            return rockBeatScissors;
+        } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+            humanWin++;
+            return scissorsBeatPaper;
+        } else if (playerSelection === "Paper" && computerSelection === "Rock") {
+            humanWin++;
+            return paperBeatRock;
+        } else {
+            computerWin++;
+            return rockBeatScissorsLoss ||  scissorsBeatPaperLoss   ||  paperBeatRockLoss;
         }
-        const choiceInLower = choice.toLowerCase();
-        if(options.includes(choiceInLower)){
-            validatedInput = true;
-            return choiceInLower;
-        }
     }
+
+    const container = document.querySelector("#container");
+    const resultsDiv = document.createElement("div");
+    resultsDiv.style.marginTop = "20px";
+    container.appendChild(resultsDiv);
+
+    
+    const humanWinText = document.createElement("p");
+    humanWinText.style.color = "#15e8af";
+    humanWinText.textContent = "Human is the Winner: " + humanWin;
+    resultsDiv.appendChild(humanWinText);
+
+    
+    const computerWinText = document.createElement("p");
+    computerWinText.style.color = "#15e8af";
+    computerWinText.textContent = "Computer conquered you: " + computerWin;
+    resultsDiv.appendChild(computerWinText);
+
+    
+    const battleWinText = document.createElement("p");
+    battleWinText.style.color = "black";
+    resultsDiv.appendChild(battleWinText);
+    
+    
+    const gameWinText = document.createElement("p");
+    gameWinText.style.color = "#15E820";
+    gameWinText.textContent = gameWinner;
+    resultsDiv.appendChild(gameWinText);
+
+    
+    function endGame() {
+        if (humanWin == 5) {
+            gameWinner = "YOU DEFEATED THE MACHINES!";
+            gameWinText.textContent = gameWinner;
+            
+            
+            document.getElementById("1").disabled = true;
+            document.getElementById("2").disabled = true;
+            document.getElementById("3").disabled = true;
+            
+            
+            const playAgainButton = document.createElement("button");
+            playAgainButton.textContent = "Battle Again!";
+            resultsDiv.appendChild(playAgainButton);
+            
+            
+            playAgainButton.addEventListener('click', () => {
+                location.reload();
+                })
+        } else if (computerWin == 5) {
+            gameWinner = "COMPUTER CONQUERED YOU!";
+            gameWinText.textContent = gameWinner;
+            
+            
+            document.getElementById("1").disabled = true;
+            document.getElementById("2").disabled = true;
+            document.getElementById("3").disabled = true;
+            
+            
+            const playAgainButton = document.createElement("button");
+            playAgainButton.textContent = "Battle Again!";
+            resultsDiv.appendChild(playAgainButton);
+            
+            
+            playAgainButton.addEventListener('click', () => {
+                location.reload();
+                })
+        }   
+    }   
+
+    
+    
+
 }
 
-function game(){
-    let scorePlayer = 0;
-    let scoreComputer = 0;
-    console.log("Ready to defeat the machines?")
-    for (let i = 0; i < 5; i++){
-        const playerSelection = getPlayerChoice();
-        const computerSelection = getComputerChoice();
-        console.log(playRound(playerSelection, computerSelection));
-        if(checkWinner(playerSelection, computerSelection) == "Player"){
-            scorePlayer++;
-        } else if(checkWinner(playerSelection, computerSelection) == "Computer"){
-            scoreComputer++;
-        }
-    }
-    console.log("Game Over!")
-    if(scorePlayer > scoreComputer){
-        console.log("The human defeated the machines!")
-    } else if(scorePlayer < scoreComputer){
-        console.log("The machines conquered the world!")
-    } else{
-        console.log("It's a tie!")
-    }
-}
-
-game()
+game();
